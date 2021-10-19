3c5d02ae3ed737a5425250b5ba3ab63a5e53b25e
GLADIK-VEDRO <60296478+GLADIK-VEDRO@users.noreply.github.com>
2021-10-19 18:36:42 +0300

    add HomeWork_1

--- HomeWork_1.py
+++ HomeWork_1.py
@@ -1,15 +1,31 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 from PyQt5.QtWidgets import QLabel, QLCDNumber
+from HM1 import Ui_MainWindow
 
 
-class Example(QWidget):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
 
-    pass
+        self.st = '->'
+        self.pushButton.setText(self.st)
+        self.pushButton.clicked.connect(self.inc_click)
+        self.textBrowser_2.setText('Фокус')
+
+    def inc_click(self):
+        if self.st == '->':
+            self.st = '<-'
+            self.pushButton.setText(self.st)
+            self.textBrowser.setText('Фокус')
+            self.textBrowser_2.clear()
+        else:
+            self.st = '->'
+            self.pushButton.setText(self.st)
+            self.textBrowser_2.setText('Фокус')
+            self.textBrowser.clear()
 
 
 if __name__ == '__main__':

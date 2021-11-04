9ff8055489374d79605f0dcd5dbd38b33696e908
GLADIK-VEDRO <60296478+GLADIK-VEDRO@users.noreply.github.com>
2021-11-04 10:52:52 +0300

    add HomeWork_5

---
+++ HomeWork_5.py
@@ -0,0 +1,78 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QLabel, QLCDNumber
+from HM_5 import Ui_MainWindow
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.count = 0
+        self.radioButton.setChecked(True)
+        self.lst = ['1', '2', '3',
+                    '4', '5', '6',
+                    '7', '8', '9']
+
+        self.lst_button = [self.pushButton, self.pushButton_2, self.pushButton_3,
+               self.pushButton_4, self.pushButton_5, self.pushButton_6,
+               self.pushButton_7, self.pushButton_8, self.pushButton_9]
+
+        for i in self.lst_button:
+            i.clicked.connect(lambda ch, name=i: self.run(name))
+
+        self.pushButton_10.clicked.connect(self.newgame)
+
+    def run(self, name):
+
+        self.count += 1
+        name.setEnabled(False)
+
+        if self.radioButton.isChecked() and self.count % 2 != 0:
+            name.setText('x')
+            self.lst[self.lst_button.index(name)] = 'x'
+        elif not self.radioButton_2.isChecked():
+            name.setText('o')
+            self.lst[self.lst_button.index(name)] = 'o'
+
+        if self.radioButton_2.isChecked() and self.count % 2 != 0:
+            name.setText('o')
+            self.lst[self.lst_button.index(name)] = 'o'
+        elif not self.radioButton.isChecked():
+            name.setText('x')
+            self.lst[self.lst_button.index(name)] = 'x'
+
+        if self.check_win(self.lst):
+            self.label.setText(f'Победил {self.check_win(self.lst)}!')
+            for i in self.lst_button:
+                i.setEnabled(False)
+            self.lst = ['1', '2', '3',
+                        '4', '5', '6',
+                        '7', '8', '9']
+            self.count = 0
+
+        if self.count == 9:
+            self.label.setText('Ничья')
+
+    def newgame(self):
+        for i in self.lst_button:
+            i.setText('')
+        self.label.setText('')
+        for i in self.lst_button:
+            i.setEnabled(True)
+
+    def check_win(self, board):
+        win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        for each in win_coord:
+            if board[each[0]] == board[each[1]] == board[each[2]]:
+                return board[each[0]]
+        return False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())

400ee577c37cad1cee49d0268067b241d2e81635
GLADIK-VEDRO <60296478+GLADIK-VEDRO@users.noreply.github.com>
2022-01-18 19:41:55 +0300

    add HomeWork_19, HomeWork_20, HomeWork_21, HomeWork_22, HomeWork_23

---
+++ HomeWork_20.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+connection = sqlite3.connect("films.sqlite")
+cursor = connection.cursor()
+result = cursor.execute(""" SELECT title FROM filmsWHERE title LIKE "%Астерикс%" AND title NOT LIKE "%Обеликс%" """).fetchall()
+for i in result:
+    print(i[0])
---
+++ HomeWork_23.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+connection = sqlite3.connect("films.sqlite")
+cursor = connection.cursor()
+result = cursor.execute(""" SELECT title FROM films WHERE duration >= 85""").fetchall()
+for i in result:
+    print(i[0])
